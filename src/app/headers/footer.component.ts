e8efa9524a7b9a3650ce2c5b6df0b44de87fd311
Carlos Villanueva <cvillanueva@myafo.com>
2022-06-23 20:51:33 -0500

    footer socials add

--- src/app/headers/footer.component.ts
+++ src/app/headers/footer.component.ts
@@ -18,22 +18,31 @@ import { Component, OnInit } from '@angular/core';
   `],
   template: `
     <footer >
-      <div class="container d-flex flex-wrap justify-content-between align-items-center">
-        <div class="col-md-4 mb-0 ">
-          <p>&copy; 2022 Villanueva, Inc</p>
-          <p >Made by Carlos Villanueva</p>
+      <div class="container">
+        <div class="col-md-4 mb-0 text-center">
+            <p><img src="../favicon.ico" atl="Website Logo"></p>
+            <p>&copy; 2022 beautylashesbygabby LLC</p>
+            <p>Made by Carlos Villanueva</p>
         </div>
         
-        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-          <img src="../favicon.ico" atl="Website Logo">
-        </a>
+        <div class="col-md-4 mb-0 text-center">
+            <label>Business Hours:</label>
+        </div>
         
-        <ul class="nav col-md-4 justify-content-md-end justify-content-center">
-          <li class="nav-item"><a href="/" class="nav-link px-2">Services <i class="fa fa-house-user"></i></a></li>
-          <li class="nav-item"><a href="#portfolio" class="nav-link px-2">Gallery <i class="fa fa-briefcase"></i></a></li>
-          <li class="nav-item"><a [routerLink]="['/resume']" class="nav-link px-2">FAQs <i class="fa fa-newspaper"></i></a></li>
-          <li class="nav-item"><a href="#contact" class="nav-link px-2">Policies <span class="fa fa-address-card"></span></a></li>
-        </ul>
+        <div class="col-md-4 text-center">
+
+            <div class="template-demo text-center mb-2"> 
+                <button class="btn btn-social-icon btn-github"><a href="https://github.com/carvillanueva" target="_blank"><i class="fab fa-github"></i></a></button> 
+                <button class="btn btn-social-icon btn-linkedin"><a href="https://www.linkedin.com/in/carlosivillanueva/" target="_blank"><i class="fa fa-linkedin"></i></a></button> 
+                <button class="btn btn-social-icon btn-instagram"><a href="https://instagram.com" target="_blank"><i class="fa fa-instagram"></i></a></button>
+                <button data-toggle="modal" data-target="#contact" class="btn btn-social-icon btn-other "><i class="fas fa-envelope"></i></button>
+                <button class="btn btn-social-icon btn-other"><a href="../assets/TEST.pdf" download="TEST.pdf"><i class="fas fa-file-pdf"></i></a></button>
+            </div>
+            <!-- <li class="nav-item"><a href="/" class="nav-link px-2">Services <i class="fa fa-house-user"></i></a></li>
+            <li class="nav-item"><a href="#portfolio" class="nav-link px-2">Gallery <i class="fa fa-briefcase"></i></a></li>
+            <li class="nav-item"><a [routerLink]="['/resume']" class="nav-link px-2">FAQs <i class="fa fa-newspaper"></i></a></li>
+            <li class="nav-item"><a href="#contact" class="nav-link px-2">Policies <span class="fa fa-address-card"></span></a></li> -->
+        </div>
       </div>
     </footer>
   `
