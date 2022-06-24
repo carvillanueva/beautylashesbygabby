c080a33d37d70923f4a0e3d0a745bf0931b97d85
Carlos Villanueva <cvillanueva@myafo.com>
2022-06-23 20:42:31 -0500

    footer/header changes

--- src/app/headers/footer.component.ts
+++ src/app/headers/footer.component.ts
@@ -1,21 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'main-footer',
-    styles: [`
-
-    `],
-    template: `
-    
-    `
+  selector: 'main-footer',
+  styles: [`
+    footer {
+      padding-top: 100px;
+      padding-bottom: 100px;
+      background-color: #FFCFF2;
+      color:white;
+    }
+    a {
+      color: white;
+    }
+    a:hover {
+      color: grey;
+    }
+  `],
+  template: `
+    <footer >
+      <div class="container d-flex flex-wrap justify-content-between align-items-center">
+        <div class="col-md-4 mb-0 ">
+          <p>&copy; 2022 Villanueva, Inc</p>
+          <p >Made by Carlos Villanueva</p>
+        </div>
+        
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+          <img src="../favicon.ico" atl="Website Logo">
+        </a>
+        
+        <ul class="nav col-md-4 justify-content-md-end justify-content-center">
+          <li class="nav-item"><a href="/" class="nav-link px-2">Services <i class="fa fa-house-user"></i></a></li>
+          <li class="nav-item"><a href="#portfolio" class="nav-link px-2">Gallery <i class="fa fa-briefcase"></i></a></li>
+          <li class="nav-item"><a [routerLink]="['/resume']" class="nav-link px-2">FAQs <i class="fa fa-newspaper"></i></a></li>
+          <li class="nav-item"><a href="#contact" class="nav-link px-2">Policies <span class="fa fa-address-card"></span></a></li>
+        </ul>
+      </div>
+    </footer>
+  `
 })
 
 
 export class FooterComponent implements OnInit {
 
-    constructor( ) { }
+  constructor( ) { }
 
-    ngOnInit() { }
+  ngOnInit() { }
 
 
 }
--- src/app/headers/header.component.ts
+++ src/app/headers/header.component.ts
@@ -1,21 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'main-header',
-    styles: [`
-    
-    `],
-    template: `
-    
-    `
+  selector: 'main-header',
+  styles: [`
+    .nav-masthead .nav-link {
+      color: slategrey;
+      border-bottom: 2px solid transparent;
+    }
+    .nav-masthead .nav-link:hover,
+    .nav-masthead .nav-link:focus {
+      border-bottom-color: hotpink;
+      color: white;
+    }
+    .nav-masthead .nav-link + .nav-link {
+      margin-left: 1rem;
+    }
+    .book-btn {
+        border: solid 2px hotpink;
+        border-radius: 5px;
+    }
+  `],
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#FFCFF2;">
+      <div class="container">
+        <a class="py-2 navbar-brand" [routerLink]="['']"><img src="../favicon.ico" atl="Website Logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup" >
+          <div class="navbar-nav nav-masthead mx-auto text-center">
+            <a class="mx-5 nav-link" [routerLink]="['/']">Services</a>
+            <a class="mx-5 nav-link" [routerLink]="['/']">Gallery</a>
+            <a class="mx-5 nav-link" [routerLink]="['/']">FAQs</a>
+            <a class="mx-5 nav-link" [routerLink]="['/']">Policies</a>
+            <a class="mx-5 nav-link book-btn" [routerLink]="['/']">Book Now!</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  `
 })
 
 
 export class HeaderComponent implements OnInit {
 
-    constructor( ) { }
+  constructor( ) { }
 
-    ngOnInit() { }
+  ngOnInit() { }
 
 
 }
