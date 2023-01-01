8bc89663f02f511586a468e905ddd76622effdc0
Carlos Villanueva <cvillanueva@myafo.com>
2023-01-01 11:47:17 -0600

    update booking iframe

--- src/app/home/book.component.ts
+++ src/app/home/book.component.ts
@@ -30,7 +30,8 @@ import { Component, OnInit } from '@angular/core';
     <!-- appointment -->
     <div class="container col-xs-12 mb-5 after-banner-fix">
       <!-- <iframe src="https://app.squarespacescheduling.com/schedule.php?owner=26839068" title="Schedule Appointment" width="100%" height="800" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script> -->
-      <!-- Start Square Appointments Embed Code --><iframe src="https://square.site/appointments/buyer/widget/tp9rp5dcovtycx/LZN8YED0Q3V12" title="Schedule Appointment" width="100%" height="800" frameBorder="0"></iframe><script src='https://square.site/appointments/buyer/widget/tp9rp5dcovtycx/LZN8YED0Q3V12.js'></script><!-- End Square Appointments Embed Code -->
+      <!-- Start Square Appointments Embed Code<iframe src="https://square.site/appointments/buyer/widget/tp9rp5dcovtycx/LZN8YED0Q3V12" title="Schedule Appointment" width="100%" height="800" frameBorder="0"></iframe><script src='https://square.site/appointments/buyer/widget/tp9rp5dcovtycx/LZN8YED0Q3V12.js'></script>End Square Appointments Embed Code -->
+      <iframe src="https://app.squarespacescheduling.com/schedule.php?owner=24678638" title="Schedule Appointment" width="100%" height="800" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
     </div>
   `
 })
